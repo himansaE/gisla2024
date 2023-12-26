1016efbdcfba2693b20079df65a16a8337741861
HimansaE <himansa.24067@gmail.com>
2023-12-27 01:38:46 +0530

    disable auth redirect

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,18 +2,17 @@ import { COMPETITION_START_DATE } from "@/lib/constants";
 import { redirect } from "next/navigation";
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    if (COMPETITION_START_DATE.getTime() > new Date().getTime() && process.env.NODE_ENV == "production")
+  if (
+    COMPETITION_START_DATE.getTime() > new Date().getTime() &&
+    process.env.NODE_ENV == "production" &&
+    !(process.env.testing == "1")
+  )
+    // TODO:: make component for registration open soon
+    return redirect("/");
 
-        // TODO:: make component for registration open soon
-        return redirect("/")
-
-    return (
-        <>
-            {children}
-        </>
-    );
+  return <>{children}</>;
 }
