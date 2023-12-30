b961d2ab2a414f6f0c9606d7be6b12350012ca76
HimansaE <himansa.24067@gmail.com>
2023-12-30 11:46:17 +0530

    fix signout redirect error

--- app/api/signout/route.ts
+++ app/api/signout/route.ts
@@ -1,9 +1,11 @@
 import { signOut } from "@/lib/auth/auth";
+import { redirect } from "next/navigation";
 
 function signout(req: Request) {
   signOut({
     redirectTo: "/",
   });
+  return redirect("/");
 }
 
-export { signOut as GET };
+export { signout as GET };
