ebf0a8f4e8ca7725df7a02d372c5adfbf14e2d15
HimansaE <himansa.24067@gmail.com>
2024-01-05 21:55:31 +0530

    chnage Reserved Variables

--- lib/aws/s3.ts
+++ lib/aws/s3.ts
@@ -7,8 +7,8 @@ import {
 export const s3 = new S3Client({
   region: "ap-southeast-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY ?? "",
-    secretAccessKey: process.env.AWS_SECRET_KEY ?? "",
+    accessKeyId: process.env.AWS_ACC_ACCESS_KEY ?? "",
+    secretAccessKey: process.env.AWS_ACC_SECRET_KEY ?? "",
   },
 });
 
