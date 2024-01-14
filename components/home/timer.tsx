349258e2b629ed976c8be8b2c7b10745d4446f95
HimansaE <himansa.24067@gmail.com>
2024-01-14 19:38:32 +0530

    remove padding

--- components/home/timer.tsx
+++ components/home/timer.tsx
@@ -9,7 +9,7 @@ export const Timer = () => {
     const now_unformatted = [
       diff / 1000,
       diff / 1000 / 60,
-      diff / 1000 / 60 / 60 - 6,
+      diff / 1000 / 60 / 60,
       diff / 1000 / 60 / 60 / 24,
     ].map((i) => Math.floor(i));
     const format = [60, 60, 24, 1000];
@@ -40,14 +40,17 @@ export const Timer = () => {
     return () => clearInterval(t);
   }, []);
 
-  if (time == null) return <></>;
   return (
     <div className="bg-bg-main rounded-xl pl-2 pb-2">
       <div className="flex tabular-nums text-white" style={font_pasti.style}>
-        <TimerNumber n={time.day} title="Days" />
-        <TimerNumber n={time.hour} title="Hours" />
-        <TimerNumber n={time.min} title="Mins" />
-        <TimerNumber n={time.sec} title="Secs" className="xsm:flex hidden " />
+        <TimerNumber n={time?.day ?? ["00", "00"]} title="Days" />
+        <TimerNumber n={time?.hour ?? ["00", "00"]} title="Hours" />
+        <TimerNumber n={time?.min ?? ["00", "00"]} title="Mins" />
+        <TimerNumber
+          n={time?.sec ?? ["00", "00"]}
+          title="Secs"
+          className="xsm:flex hidden "
+        />
       </div>
       <div
         className={`${font_poppins_one.className} text-white fon text-center text-sm sm:text-base`}
