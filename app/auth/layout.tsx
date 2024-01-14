8857e85dddabfded27237138605f914379ad5961
HimansaE <himansa.24067@gmail.com>
2024-01-14 20:16:25 +0530

    fix

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,18 +1,7 @@
-import { COMPETITION_START_DATE } from "@/lib/constants";
-import { redirect } from "next/navigation";
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  if (
-    COMPETITION_START_DATE.getTime() > new Date().getTime() &&
-    process.env.NODE_ENV == "production" &&
-    !(process.env.testing == "1")
-  )
-    // TODO:: make component for registration open soon
-    return redirect("/");
-
   return <>{children}</>;
 }
